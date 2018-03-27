e4ced96c269c4d5244b608153a526a7ebc0adf95
adrianajimeneznava <adrianajimeneznava@192.168.100.2>
2018-03-27 02:14:10 -0600

    #3 Menú y pantallas

--- src/textilesJimenez/menuTextiles.java
+++ src/textilesJimenez/menuTextiles.java
@@ -2,6 +2,9 @@ package textilesJimenez;
 
 public class menuTextiles extends javax.swing.JFrame {
 
+    /**
+     * Creates new form menuTextiles
+     */
     public menuTextiles() {
         initComponents();
         this.setLocationRelativeTo(null);
@@ -17,103 +20,129 @@ public class menuTextiles extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel2 = new javax.swing.JLabel();
-        pedidos = new javax.swing.JButton();
-        bordado = new javax.swing.JButton();
-        factHilo = new javax.swing.JButton();
-        pagosT = new javax.swing.JButton();
-        cuenta = new javax.swing.JButton();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        btnModficarPedidos = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        btnModificarTrabajadores = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        jButton4 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        menuSalir = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setFont(new java.awt.Font("Oriya MN", 1, 50)); // NOI18N
-        jLabel2.setText("Textiles Jimenez");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 40, -1, -1));
+        jLabel2.setFont(new java.awt.Font("Kohinoor Bangla", 1, 50)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Textiles Jiménez");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 30, -1, -1));
 
-        pedidos.setBackground(new java.awt.Color(0, 0, 0));
-        pedidos.setFont(new java.awt.Font("Oriya MN", 0, 16)); // NOI18N
-        pedidos.setForeground(new java.awt.Color(255, 255, 255));
-        pedidos.setText("Pedidos");
-        pedidos.addActionListener(new java.awt.event.ActionListener() {
+        jTabbedPane1.setBackground(new java.awt.Color(255, 255, 255));
+        jTabbedPane1.setFont(new java.awt.Font("Khmer Sangam MN", 0, 15)); // NOI18N
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnModficarPedidos.setBackground(new java.awt.Color(0, 0, 0));
+        btnModficarPedidos.setFont(new java.awt.Font("Khmer Sangam MN", 0, 16)); // NOI18N
+        btnModficarPedidos.setForeground(new java.awt.Color(255, 255, 255));
+        btnModficarPedidos.setText("Modificar");
+        btnModficarPedidos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pedidosActionPerformed(evt);
+                btnModficarPedidosActionPerformed(evt);
             }
         });
-        getContentPane().add(pedidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 190, 160, 60));
+        jPanel1.add(btnModficarPedidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 60, 110, 40));
 
-        bordado.setBackground(new java.awt.Color(0, 0, 0));
-        bordado.setFont(new java.awt.Font("Oriya MN", 0, 16)); // NOI18N
-        bordado.setForeground(new java.awt.Color(255, 255, 255));
-        bordado.setText("Bordado");
-        bordado.addActionListener(new java.awt.event.ActionListener() {
+        jTabbedPane1.addTab("Pedidos", jPanel1);
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnModificarTrabajadores.setBackground(new java.awt.Color(0, 0, 0));
+        btnModificarTrabajadores.setFont(new java.awt.Font("Khmer Sangam MN", 0, 16)); // NOI18N
+        btnModificarTrabajadores.setForeground(new java.awt.Color(255, 255, 255));
+        btnModificarTrabajadores.setText("Pagos");
+        btnModificarTrabajadores.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bordadoActionPerformed(evt);
+                btnModificarTrabajadoresActionPerformed(evt);
             }
         });
-        getContentPane().add(bordado, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 190, 160, 60));
+        jPanel2.add(btnModificarTrabajadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 60, 110, 40));
 
-        factHilo.setBackground(new java.awt.Color(0, 0, 0));
-        factHilo.setFont(new java.awt.Font("Oriya MN", 0, 16)); // NOI18N
-        factHilo.setForeground(new java.awt.Color(255, 255, 255));
-        factHilo.setText("Fact. Hilo");
-        factHilo.addActionListener(new java.awt.event.ActionListener() {
+        jTabbedPane1.addTab("Trabajadores", jPanel2);
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton3.setBackground(new java.awt.Color(0, 0, 0));
+        jButton3.setFont(new java.awt.Font("Khmer Sangam MN", 0, 16)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("Modificar");
+        jPanel3.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 60, 110, 40));
+
+        jTabbedPane1.addTab("Préstamos", jPanel3);
+
+        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton4.setBackground(new java.awt.Color(0, 0, 0));
+        jButton4.setFont(new java.awt.Font("Khmer Sangam MN", 0, 16)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(255, 255, 255));
+        jButton4.setText("Modificar");
+        jPanel4.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 60, 110, 40));
+
+        jTabbedPane1.addTab("Materia prima", jPanel4);
+
+        getContentPane().add(jTabbedPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 150, 430, 220));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesSistema/fondoMorado1.jpg"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 680, 450));
+
+        jMenuBar1.setPreferredSize(new java.awt.Dimension(50, 23));
+        jMenuBar1.setSize(new java.awt.Dimension(86, 18));
+
+        jMenu1.setText("Inicio");
+        jMenu1.setFont(new java.awt.Font("Khmer Sangam MN", 0, 14)); // NOI18N
+
+        menuSalir.setFont(new java.awt.Font("Khmer Sangam MN", 0, 14)); // NOI18N
+        menuSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesSistema/btnSalir.png"))); // NOI18N
+        menuSalir.setText("Salir");
+        menuSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                factHiloActionPerformed(evt);
+                menuSalirActionPerformed(evt);
             }
         });
-        getContentPane().add(factHilo, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 300, 160, 60));
+        jMenu1.add(menuSalir);
 
-        pagosT.setBackground(new java.awt.Color(0, 0, 0));
-        pagosT.setFont(new java.awt.Font("Oriya MN", 0, 16)); // NOI18N
-        pagosT.setForeground(new java.awt.Color(255, 255, 255));
-        pagosT.setText("Pago trabajadores");
-        pagosT.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pagosTActionPerformed(evt);
-            }
-        });
-        getContentPane().add(pagosT, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 300, 200, 60));
+        jMenuBar1.add(jMenu1);
 
-        cuenta.setBackground(new java.awt.Color(0, 0, 0));
-        cuenta.setFont(new java.awt.Font("Oriya MN", 0, 16)); // NOI18N
-        cuenta.setForeground(new java.awt.Color(255, 255, 255));
-        cuenta.setText("Cuenta");
-        cuenta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cuentaActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 420, 160, 60));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesSistema/fondo.jpg"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1010, 590));
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void pedidosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pedidosActionPerformed
-        pedidos pedidos1 = new pedidos();
-        pedidos1.setVisible(true);
+    private void menuSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSalirActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_menuSalirActionPerformed
+
+    private void btnModficarPedidosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModficarPedidosActionPerformed
+        modificarPedidos modPedidos = new modificarPedidos();
         this.setVisible(false);
-    }//GEN-LAST:event_pedidosActionPerformed
+        modPedidos.setVisible(true);
+    }//GEN-LAST:event_btnModficarPedidosActionPerformed
 
-    private void bordadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bordadoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_bordadoActionPerformed
-
-    private void factHiloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_factHiloActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_factHiloActionPerformed
-
-    private void pagosTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pagosTActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_pagosTActionPerformed
-
-    private void cuentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cuentaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cuentaActionPerformed
+    private void btnModificarTrabajadoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarTrabajadoresActionPerformed
+        pagoTrabajadores pagoTra = new pagoTrabajadores();
+        this.setVisible(false);
+        pagoTra.setVisible(true);
+    }//GEN-LAST:event_btnModificarTrabajadoresActionPerformed
 
     /**
      * @param args the command line arguments
@@ -151,12 +180,19 @@ public class menuTextiles extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton bordado;
-    private javax.swing.JButton cuenta;
-    private javax.swing.JButton factHilo;
+    private javax.swing.JButton btnModficarPedidos;
+    private javax.swing.JButton btnModificarTrabajadores;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JButton pagosT;
-    private javax.swing.JButton pedidos;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JMenuItem menuSalir;
     // End of variables declaration//GEN-END:variables
 }
